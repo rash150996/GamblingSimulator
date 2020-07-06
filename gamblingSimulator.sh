463c217644cd58b2f99b6d9f71b8205c8fdafd2a
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-07-06 17:43:53 +0530

    [rashmi] Modified functionality of winning or losing over 20 days of gambling

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -2,16 +2,29 @@
 echo "Welcome to Gambling Simulator"
 stakePerDay=100
 betPerGame=1
-while (( $stakePerDay > 50 && $stakePerDay < 150 ))
+daysPlayed=0
+total=0
+
+while (( $daysPlayed < 20 ))
 do
- flip=$(( RANDOM % 2 ))
- if [ $flip -eq 1 ]
- then
+	stakePerDay=100
+	while (( $stakePerDay > 50 && $stakePerDay < 150 ))
+	do
+ 	flip=$(( RANDOM % 2 ))
+ 	if [ $flip -eq 1 ]
+ 	then
         stakePerDay=$(( $stakePerDay + $betPerGame ))
- else
+ 	else
         stakePerDay=$(( $stakePerDay - $betPerGame ))
- fi
+ 	fi
+	done
+   total=$(( $total + $stakePerDay ))
+	daysPlayed=$(( $daysPlayed + 1 ))
 done
 
-echo "you are left with rs.$stakePerDay"
-
+if [ $total > 2000 ]
+then
+	echo "you are left with \$$total on the winning side"
+else
+	echo "you are left with \$$total on the losing side"
+fi
