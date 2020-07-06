bf03068c4b501005009e809a5e25db2c50aacc33
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-07-06 15:27:45 +0530

    [rashmi] Added declaration for stake everyday and bet per game

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Welcome to Gambling Simulator"
+stakePerDay=100
+betPerGame=1
+echo "Stakes per day would be \$$stakePerDay "
+echo "Bet to be placed per game will be \$$betPerGame"
 
