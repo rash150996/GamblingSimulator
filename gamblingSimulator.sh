cfae994ab5d52ab1e2205ff0a46a6a236a96f28e
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-07-06 17:23:23 +0530

    [rashmi] Modified functionality of winning or loosing by adding exit strategy of 50% stake

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -2,14 +2,16 @@
 echo "Welcome to Gambling Simulator"
 stakePerDay=100
 betPerGame=1
-flip=$(( RANDOM % 2 ))
-if [ $flip -eq 1 ]
-then
-	echo "You won the bet"
-	total=$(( $stakePerDay + $betPerGame ))
-else
-	echo "You lost the bet"
-	total=$(( $stakePerDay - $betPerGame ))
-fi
+while (( $stakePerDay > 50 && $stakePerDay < 150 ))
+do
+ flip=$(( RANDOM % 2 ))
+ if [ $flip -eq 1 ]
+ then
+        stakePerDay=$(( $stakePerDay + $betPerGame ))
+ else
+        stakePerDay=$(( $stakePerDay - $betPerGame ))
+ fi
+done
 
-echo "You are left with \$$total"
+echo "you are left with rs.$stakePerDay"
+
